2fd8f60e02b1acc1a764510e1f7cbdf79fde82d5
JoakimJoreng <jjoereng@gmail.com>
2014-09-25 15:23:01 +0200

    Bubblecloud must die

--- app/src/main/java/imt3662/hig/no/bubbles/MainActivity.java
+++ app/src/main/java/imt3662/hig/no/bubbles/MainActivity.java
@@ -26,7 +26,8 @@ public class MainActivity extends Activity {
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
             }
         });
 
