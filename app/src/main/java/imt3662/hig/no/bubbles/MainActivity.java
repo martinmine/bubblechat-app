1c14c5bdb2142fac9f1f88748af2521c3c99030d
JoakimJoreng <jjoereng@gmail.com>
2014-09-25 11:20:53 +0200

    Added prototype for map

--- app/src/main/java/imt3662/hig/no/bubbles/MainActivity.java
+++ app/src/main/java/imt3662/hig/no/bubbles/MainActivity.java
@@ -1,9 +1,14 @@
 package imt3662.hig.no.bubbles;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +17,43 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button settings = (Button) findViewById(R.id.settingsButton);
+        Button goMap = (Button) findViewById(R.id.mapButton);
+
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        goMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double latitude;
+                double longitude;
+
+                EditText lati = (EditText) findViewById(R.id.lati);
+                latitude = Double.parseDouble(lati.getText().toString());
+                EditText longi = (EditText) findViewById(R.id.longi);
+                longitude = Double.parseDouble(longi.getText().toString());
+
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("LATITUDE", latitude);
+                intent.putExtra("LONGITUDE", longitude);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
     }
 
 
