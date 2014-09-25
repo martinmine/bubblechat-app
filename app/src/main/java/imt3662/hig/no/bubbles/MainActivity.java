f731b112c18d0ad3dd33ba58f02b2287ea246a7b
JoakimJoreng <jjoereng@gmail.com>
2014-09-25 17:04:00 +0200

    Settings is still a work in progress

--- app/src/main/java/imt3662/hig/no/bubbles/MainActivity.java
+++ app/src/main/java/imt3662/hig/no/bubbles/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
